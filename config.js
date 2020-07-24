2ed2789a8ac0425c163e42a830bddcc08bed111a
Arnoud de Boer <a.deboer@geonovum.nl>
2020-07-24 10:16:25 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,38 @@
+var respecConfig = {
+  specStatus: "GN-WV", // specStatus: "GN-WV", "GN-CV", "GN-VV of "GN-DEF" ,
+  specType: "IM",
+    maxTocLevel: 4,
+  // publishDate: "2019-05-08", 
+  logos: [
+  {src: 'https://docs.geostandaarden.nl/bro/gen/logo_bro.png',
+    href: "http://basisregistratieondergrond.nl",
+    alt: "Basisregistratie Ondergrond",
+    width: 252,
+    height: 130,
+    id: 'logo_bro'},
+  {
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 776,
+    height: 163,
+    id: 'logo_minbzk',
+  }]  ,
+   editors: [    {
+      name: "Geonovum",
+	    companyURL: "https://www.geonovum.nl/geo-standaarden/bro-basisregistratie-ondergrond",
+    }],
+  authors: [
+    {
+      name: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/bro-basisregistratie-ondergrond",
+    }
+	],
+  shortName: "SGM", // mnemonic
+  pubDomain: "bro",
+  github: "https://github.com/BROprogramma/SGM",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  localBiblio: {
+  },
+  };
